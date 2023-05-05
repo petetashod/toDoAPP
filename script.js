87e710da66ce3c67c7c2cc5912cd5e558ccf4540
tashod peterson <tashodpeterson@tashods-Air.lan>
2023-05-04 23:29:20 -0400

    Final

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
-const button = document.getElementById('button');
-const input = document.getElementById('input');
-const toDoList = document.getElementById('actualToDoList');
 
-button.addEventListener('click', function(){
-    let listOfInformation = document.createElement('p');
-    listOfInformation.textContent = input.value;
-    toDoList.appendChild(listOfInformation);
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const toDoList = document.getElementById("actualToDoList");
+const removeBtn = document.getElementById("removeBtn");
 
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  let listOfInformation = document.createElement("p");
+  listOfInformation.textContent = input.value;
+  toDoList.appendChild(listOfInformation);
+  input.value = "";
 
-})
+  listOfInformation.addEventListener("click", function () {
+    listOfInformation.style.textDecoration = "line-through";
+  });
+  removeBtn.addEventListener("click", function () {
+    toDoList.removeChild(listOfInformation);
+  });
+});
