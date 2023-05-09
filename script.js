86dcc0a1c6ab295350bcd341ad4df88af84f14bd
tashod peterson <tashodpeterson@tashods-MacBook-Air.local>
2023-05-08 22:24:47 -0400

    Final

--- script.js
+++ script.js
@@ -3,20 +3,24 @@
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const toDoList = document.getElementById("actualToDoList");
-const removeBtn = document.getElementById("removeButton");
-
 button.addEventListener("click", function (e) {
   e.preventDefault();
-  let listOfInformation = document.createElement("li");
-  listOfInformation.textContent = input.value;
-  toDoList.appendChild(listOfInformation);
+  let listOfInformation = document.createElement("div");
+  listOfInformation.className = "containerOfEachTask";
+  let pElement = document.createElement("p");
+  pElement.textContent = input.value;
+  listOfInformation.appendChild(pElement);
+  let removeButton = document.createElement("button");
+  removeButton.innerText = "x";
+  removeButton.setAttribute('class', 'removeButton');
+  let task = toDoList.appendChild(listOfInformation);
+  listOfInformation.appendChild(removeButton);
   input.value = "";
-
-  listOfInformation.addEventListener("click", function () {
-    listOfInformation.style.textDecoration = "line-through";
+  pElement.addEventListener("click", function () {
+    pElement.style.textDecoration = "line-through";
   });
-  removeBtn.addEventListener("click", function () {
- 
-    toDoList.removeChild(listOfInformation)
+  removeButton.addEventListener("click", function removeItems() {
+     task.remove();
+    // toDoList.removeChild(listOfInformation)
   });
 });
